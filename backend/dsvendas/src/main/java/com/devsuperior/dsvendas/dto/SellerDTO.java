e6959993c6f67337d8e41f6c26734e1a622ed671
wemerson maduro <lagoawb@gmail.com>
2021-09-10 21:37:27 -0300

    Layers

--- backend/dsvendas/src/main/java/com/devsuperior/dsvendas/dto/SellerDTO.java
+++ backend/dsvendas/src/main/java/com/devsuperior/dsvendas/dto/SellerDTO.java
@@ -5,15 +5,13 @@ import java.io.Serializable;
 import com.devsuperior.dsvendas.entities.Seller;
 
 public class SellerDTO implements Serializable {
-
 	private static final long serialVersionUID = 1L;
+	
 	private Long id;
 	private String name;
 	
-	public SellerDTO() {
-		
+	public SellerDTO() {		
 	}
-
 	
 	public SellerDTO(Long id, String name) {
 		this.id = id;
